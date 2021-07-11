5ac8b05448a4178e24009536114dede24f36671a
jgarcia6 <jgarcia6@werfen.com>
2021-07-11 13:21:44 +0200

    Add read plateau method. Remove unnecessary files: out folder Update gitignore file to exclude out folder.

--- app/src/main/java/com/mower/application/CommandConsole.java
+++ app/src/main/java/com/mower/application/CommandConsole.java
@@ -7,6 +7,7 @@ import com.mower.domain.valueobjects.Coordinates;
 import com.mower.domain.valueobjects.FaceTo;
 
 import java.util.List;
+import java.util.Scanner;
 
 import static com.mower.domain.CardinalPoint.NORTH;
 import static com.mower.domain.Command.LEFT;
@@ -16,8 +17,33 @@ import static com.mower.domain.Command.RIGHT;
 
 public class CommandConsole {
 
+  private static final String USER_EXPLANATION_FOR_PLATEAU_COORDINATES = "Insert the upper right coordinates of the plateau (like: 5 5): ";
+  private static final String PLATEAU_SEPARATOR_REGEX = " ";
+  private static final String PLATEAU_REGEX = "[0-9]+" + PLATEAU_SEPARATOR_REGEX + "[0-9]+";
+  private static final int PLATEAU_WIDTH_INDEX = 0;
+  private static final int PLATEAU_HEIGHT_INDEX = 1;
+
+  private final Scanner scanner;
+
+  public CommandConsole(Scanner scanner) {
+    this.scanner = scanner;
+  }
+
   public Plateau readPlateau() {
-    return new Plateau(5, 5);
+    var plateauSize = parseArgument(USER_EXPLANATION_FOR_PLATEAU_COORDINATES, PLATEAU_REGEX);
+    return processPlateauWith(plateauSize);
+  }
+
+  private Plateau processPlateauWith(String size) {
+    return new Plateau(plateauWidthFrom(size), plateauHeightFrom(size));
+  }
+
+  private int plateauWidthFrom(String size) {
+    return Integer.parseInt(size.split(PLATEAU_SEPARATOR_REGEX)[PLATEAU_WIDTH_INDEX]);
+  }
+
+  private int plateauHeightFrom(String size) {
+    return Integer.parseInt(size.split(PLATEAU_SEPARATOR_REGEX)[PLATEAU_HEIGHT_INDEX]);
   }
 
   public Mower readMower() {
@@ -36,4 +62,20 @@ public class CommandConsole {
     System.out.printf("Mower situation: %s%n", mower.situation());
   }
 
+  private String parseArgument(String userExplanation, String validationRegex) {
+    String argument;
+    do {
+      argument = askForArgumentWith(userExplanation);
+    } while (!validateArgument(validationRegex, argument));
+    return argument;
+  }
+
+  private String askForArgumentWith(String userExplanation) {
+    System.out.print(userExplanation);
+    return scanner.nextLine();
+  }
+
+  private boolean validateArgument(String validationRegex, String argument) {
+    return argument.matches(validationRegex);
+  }
 }
--- app/src/test/java/com/mower/application/CommandConsoleShould.java
+++ app/src/test/java/com/mower/application/CommandConsoleShould.java
@@ -6,26 +6,47 @@ import com.mower.domain.valueobjects.Coordinates;
 import com.mower.domain.valueobjects.FaceTo;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.internal.verification.Times;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Scanner;
 
 import static com.mower.domain.CardinalPoint.NORTH;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 public class CommandConsoleShould {
 
   private static final int ANY_COORDINATE_X = 1;
   private static final int ANY_COORDINATE_Y = 2;
   private static final CardinalPoint ANY_CARDINAL_POINT = NORTH;
+  private static final String VALID_INPUT_PLATEAU_DIMENSIONS = "5 5";
+  private static final String INVALID_INPUT_PLATEAU_DIMENSIONS = "INVALID DIMENSIONS";
 
+  @Mock
+  Scanner scannerMocked;
   private CommandConsole commandConsole;
 
   @BeforeEach
   void setUp() {
-    commandConsole = new CommandConsole();
+    commandConsole = new CommandConsole(scannerMocked);
   }
 
   @Test
   void readPlateau() {
+    when(scannerMocked.nextLine()).thenReturn(VALID_INPUT_PLATEAU_DIMENSIONS);
     assertThat(commandConsole.readPlateau()).isNotNull();
+  }
+
+  @Test
+  void readPlateauWithErrorFirst() {
+    when(scannerMocked.nextLine()).thenReturn(INVALID_INPUT_PLATEAU_DIMENSIONS, VALID_INPUT_PLATEAU_DIMENSIONS);
+    assertThat(commandConsole.readPlateau()).isNotNull();
+    verify(scannerMocked, new Times(2)).nextLine();
   }
 
   @Test
